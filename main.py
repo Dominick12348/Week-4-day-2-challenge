40298ac8020817de1808102d80d91c17599f9cdb
Dominick12348 <122392919+Dominick12348@users.noreply.github.com>
2023-09-13 14:06:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+Name = input("What is your name? ")
+sales = int(input("How much money in sales have you made? "))
+comissions = (sales * 0.13)
+print(f"Your name is {Name} and you are entitled to {comissions} dollars of commissions")
